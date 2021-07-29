f3b46bc0a9b77ac432bf86f6291dafaa401d4df4
Inżynier Domu <inzynier.domu@gmail.com>
2021-07-29 23:37:16 +0200

    fix naming, add function to calculate minutes

--- src/main.cpp
+++ src/main.cpp
@@ -15,8 +15,20 @@ RTC_DS1307 m_rtc; ///< DS1307 RTC
 const DateTime m_start = DateTime(2000, 1, 1, 16, 48, 0); ///< time when realy turn on
 const DateTime m_stop = DateTime(2000, 1, 1, 16, 50, 0);  ///< time when realy turn off
 const byte m_output_pin = 2;        ///< pin connected to relay
-const int min_in_h = 60;            ///< minutes in an hour
-const int refresh_time_ms = 15000;  ///< time of repeting check time is in range and sending message
+const int m_min_in_h = 60;          ///< minutes in an hour
+const int m_refresh_time_ms = 15000;  ///< time of repeting check time is in range and sending message
+
+/**
+ * @brief calculate hours and minutes to only minutes
+ * @param time hours and minutes to calculate
+ * @return minutes
+ */
+int min_calculate(const DateTime& time)
+{
+  int time_minute = time.hour() * m_min_in_h;
+  time_minute += time.minute();
+  return time_minute;
+}
 
 /**
  * @brief check if current time is in range from start to end time
@@ -27,14 +39,9 @@ const int refresh_time_ms = 15000;  ///< time of repeting check time is in range
  */
 bool is_in_range(const DateTime& current, const DateTime& start, const DateTime& end)
 {
-  int current_minute = current.hour() * min_in_h;
-  current_minute += current.minute();
-
-  int start_minute = start.hour() * min_in_h;
-  start_minute += start.minute();
-
-  int end_minute = end.hour() * min_in_h;
-  end_minute += end.minute();
+  int current_minute = min_calculate(current);
+  int start_minute = min_calculate(start);
+  int end_minute = min_calculate(end);
 
   return((current_minute >= start_minute) && (current_minute < end_minute));
 }
@@ -68,7 +75,7 @@ void loop()
 {
   static long last_loop_time = 0;
   long loop_time = millis();
-  if(loop_time - last_loop_time > refresh_time_ms)
+  if(loop_time - last_loop_time > m_refresh_time_ms)
   {
     DateTime time = m_rtc.now();
     Serial.print(time.hour(), DEC);
