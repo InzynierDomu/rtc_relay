747c91bb59e9c63b46613859e4076bb2ab96ca30
Inżynier Domu <inzynier.domu@gmail.com>
2021-10-06 12:44:36 +0200

    change variables type

--- src/main.cpp
+++ src/main.cpp
@@ -14,18 +14,18 @@ RTC_DS1307 m_rtc; ///< DS1307 RTC
 
 const DateTime m_start = DateTime(2000, 1, 1, 16, 48, 0); ///< time when realy turn on
 const DateTime m_stop = DateTime(2000, 1, 1, 16, 50, 0);  ///< time when realy turn off
-const byte m_output_pin = 2;        ///< pin connected to relay
-const int m_min_in_h = 60;          ///< minutes in an hour
-const int m_refresh_time_ms = 15000;  ///< time of repeting check time is in range and sending message
+const byte m_output_pin = 2;          ///< pin connected to relay
+const uint8_t m_min_in_h = 60;        ///< minutes in an hour
+const unsigned long m_refresh_time_ms = 15000;  ///< time of repeting check time is in range and sending message
 
 /**
  * @brief calculate hours and minutes to only minutes
  * @param time hours and minutes to calculate
  * @return minutes
  */
-int min_calculate(const DateTime& time)
+uint16_t min_calculate(const DateTime& time)
 {
-  int time_minute = time.hour() * m_min_in_h;
+  uint16_t time_minute = time.hour() * m_min_in_h;
   time_minute += time.minute();
   return time_minute;
 }
@@ -39,9 +39,9 @@ int min_calculate(const DateTime& time)
  */
 bool is_in_range(const DateTime& current, const DateTime& start, const DateTime& end)
 {
-  int current_minute = min_calculate(current);
-  int start_minute = min_calculate(start);
-  int end_minute = min_calculate(end);
+  auto current_minute = min_calculate(current);
+  auto start_minute = min_calculate(start);
+  auto end_minute = min_calculate(end);
 
   return((current_minute >= start_minute) && (current_minute < end_minute));
 }
@@ -73,26 +73,26 @@ void setup()
  */
 void loop() 
 {
-  static long last_loop_time = 0;
-  long loop_time = millis();
+  static unsigned long last_loop_time = 0;
+  unsigned long loop_time = millis();
   if(loop_time - last_loop_time > m_refresh_time_ms)
   {
-    DateTime time = m_rtc.now();
-    Serial.print(time.hour(), DEC);
+    DateTime now = m_rtc.now();
+    Serial.print(now.hour(), DEC);
     Serial.print(":");
-    Serial.print(time.minute(), DEC);
+    Serial.print(now.minute(), DEC);
     Serial.print(":");
-    Serial.println(time.second(), DEC);
+    Serial.println(now.second(), DEC);
     last_loop_time = millis();
 
     static bool is_turn_on = false;
-    if(is_in_range(m_rtc.now(), m_start, m_stop) && !is_turn_on)
+    if(is_in_range(now, m_start, m_stop) && !is_turn_on)
     {
       Serial.println("Start");
       digitalWrite(m_output_pin, HIGH);
       is_turn_on = true;
     }
-    else if(!is_in_range(m_rtc.now(), m_start, m_stop) && is_turn_on)
+    else if(!is_in_range(now, m_start, m_stop) && is_turn_on)
     {
       Serial.println("Stop");
       digitalWrite(m_output_pin, LOW);
