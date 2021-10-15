0f77b5a92d2d7576f6bde3b766f6f4b2b6876422
Inżynier Domu <inzynier.domu@gmail.com>
2021-10-15 18:12:10 +0200

    clang format

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,18 @@
  * @date 06-2021
  */
 
+#include "RTClib.h"
+
 #include <Arduino.h>
 #include <Wire.h>
-#include "RTClib.h"
 
 RTC_DS1307 m_rtc; ///< DS1307 RTC
 
 const DateTime m_start = DateTime(2000, 1, 1, 16, 48, 0); ///< time when realy turn on
-const DateTime m_stop = DateTime(2000, 1, 1, 16, 50, 0);  ///< time when realy turn off
-const byte m_output_pin = 2;          ///< pin connected to relay
-const uint8_t m_min_in_h = 60;        ///< minutes in an hour
-const unsigned long m_refresh_time_ms = 15000;  ///< time of repeting check time is in range and sending message
+const DateTime m_stop = DateTime(2000, 1, 1, 16, 50, 0); ///< time when realy turn off
+const byte m_output_pin = 2; ///< pin connected to relay
+const uint8_t m_min_in_h = 60; ///< minutes in an hour
+const unsigned long m_refresh_time_ms = 15000; ///< time of repeting check time is in range and sending message
 
 /**
  * @brief calculate hours and minutes to only minutes
@@ -43,26 +44,26 @@ bool is_in_range(const DateTime& current, const DateTime& start, const DateTime&
   auto start_minute = min_calculate(start);
   auto end_minute = min_calculate(end);
 
-  return((current_minute >= start_minute) && (current_minute < end_minute));
+  return ((current_minute >= start_minute) && (current_minute < end_minute));
 }
 
 /**
  * @brief setup
  */
-void setup() 
+void setup()
 {
-  Serial.begin(9600);  
+  Serial.begin(9600);
 
   pinMode(m_output_pin, OUTPUT);
 
-  if (!m_rtc.begin()) 
+  if (!m_rtc.begin())
   {
     Serial.println("Couldn't find RTC");
   }
 
   if (!m_rtc.isrunning())
   {
-    Serial.println("RTC is NOT running, let's set the time!");    
+    Serial.println("RTC is NOT running, let's set the time!");
   }
 
   m_rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
@@ -71,11 +72,11 @@ void setup()
 /**
  * @brief main loop
  */
-void loop() 
+void loop()
 {
   static unsigned long last_loop_time = 0;
   unsigned long loop_time = millis();
-  if(loop_time - last_loop_time > m_refresh_time_ms)
+  if (loop_time - last_loop_time > m_refresh_time_ms)
   {
     DateTime now = m_rtc.now();
     Serial.print(now.hour(), DEC);
@@ -86,13 +87,13 @@ void loop()
     last_loop_time = millis();
 
     static bool is_turn_on = false;
-    if(is_in_range(now, m_start, m_stop) && !is_turn_on)
+    if (is_in_range(now, m_start, m_stop) && !is_turn_on)
     {
       Serial.println("Start");
       digitalWrite(m_output_pin, HIGH);
       is_turn_on = true;
     }
-    else if(!is_in_range(now, m_start, m_stop) && is_turn_on)
+    else if (!is_in_range(now, m_start, m_stop) && is_turn_on)
     {
       Serial.println("Stop");
       digitalWrite(m_output_pin, LOW);
